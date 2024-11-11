425e5b7b287ea3ac4a77439cccbc508b4d9df209
Avital-Ha <avital.hamburg@gmail.com>
2024-11-11 15:11:54 +0200

    git commit-m"תיקון קישור"

--- src/App.js
+++ src/App.js
@@ -10,9 +10,8 @@ function App() {
         <div className="App">
             <Header className="header-fixed" />
             <Routes>
-            <Route path="/" element={<Navigate to="/college" />} />
-                <Route path="/college" element={<Intro />} />
-                <Route path="/college/home" element={<Home className="home" />} />
+                <Route path="/" element={<Intro />} />
+                <Route path="/home" element={<Home className="home" />} />
             </Routes>
         </div>
     );
