f48018ec40100fcd87bd0d766154f509c0b2a8e7
Avital-Ha <avital.hamburg@gmail.com>
2025-08-27 09:31:57 +0300

    עדכון קבצים לJSX

--- src/componentsJS/Header.jsx
+++ src/componentsJS/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import '../componentsCSS/Header.css';
-import Hamburger from '../componentsJS/Hamburger';
+import Hamburger from './Hamburger';
 
 function Header() {
   const navigate = useNavigate();
